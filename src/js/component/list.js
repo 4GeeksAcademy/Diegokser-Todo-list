5877f7a0d70e9a289029c6060aa71968e7015d2c
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-19 19:23:15 +0000

    cambios en ficheros index y list

--- src/js/component/list.js
+++ src/js/component/list.js
@@ -3,17 +3,30 @@ import React, {useState} from "react";
 const AllList = () => {
     const [inputValue,setInputValue] = useState("")
     const [list,setList] = useState([])
+    
+    const removeListItem = (index) => {
+        setList((prevList) => {
+          const newList = prevList.filter((_, i) => i !== index);
+          return newList;
+        });
+      };
+
+
+    const liList = list.map((element, i) => {
+        return <li key={i}>{element}<i className="fa-solid fa-x position-absolute end-0 mx-3" onClick={() => removeListItem(i)}></i></li>
+})
+ 
+
 	return (
 		<div className="container">
             <h1>todos</h1>
-			<ul>
+			<ul className="position-relative">
                 <li><input type="text" placeholder="No hay tareas, añadir tareas" onChange={(element)=> setInputValue(element.target.value)} value={inputValue} onKeyDown={(item)=> {if(item.key === "Enter"){
                                 setList(list.concat([inputValue]));
-                                setInputValue(""); }} ></input></li>
-                <li>hola<i class="fa-solid fa-x"></i></li>
-
+                                setInputValue("");}}}></input></li>
+                <li>{liList}</li>
             </ul>
-            <div>23 task</div>
+            <div> {list.length} task</div>
 		</div>
 	);
 };
